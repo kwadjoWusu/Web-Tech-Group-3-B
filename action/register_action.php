cac8edf8a9ecdbf35412984bf11ab48bbe77dff0
g3w <abigailtetteh077@gmail.com>
2024-03-04 19:25:22 +0000

    check

--- action/register_action.php
+++ action/register_action.php
@@ -2,30 +2,63 @@
 include('../settings/connection.php');
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $firstname = $_POST["firstname"];
-    $lastname = $_POST["lastname"];
+    $fname = $_POST["fname"];
+    $lname = $_POST["lname"];
     $gender = $_POST["gender"];
+    $role = $_POST["role"];
     $major = $_POST["major"];
-    $gradclass = $_POST["gradclass"];
+    $yeargroup = $_POST["yeargroup"];
     $email = $_POST["email"];
     $password = password_hash($_POST["passwd"], PASSWORD_DEFAULT);
 
 
-    $sql = "INSERT INTO people (firstname, lastname, gender, major, gradclass, email, passwd) 
-            VALUES ('$firstname', '$lastname', '$gender', '$major', '$gradclass', '$email', '$password')";
+    $RoleQuery = "SELECT roleID FROM roles WHERE roleID = '$role'";
+    $RoleResult = $conn->query($RoleQuery);
 
-    if ($conn->query($sql) === TRUE) {
 
-            
-        header("Location: ../view/dash.php");
-        exit();
+    if ($RoleResult === FALSE) {
+        echo "Error: " . $RoleQuery . "<br>" . $conn->error;
+    } elseif ($RoleResult->num_rows > 0) {
+        $RoleRow = $RoleResult->fetch_assoc();
+        $roleID = $RoleRow["roleID"];
 
-               
+        $roleQuery = "SELECT roleID FROM roles WHERE roleID ='$role'";
+        $roleResult = $conn->query($roleQuery);
 
+
+        if ($roleResult === FALSE) {
+            echo "Error: " . $roleQuery . "<br>" . $conn->error;
+        } elseif ($roleResult->num_rows > 0) {
+            $roleRow = $roleResult->fetch_assoc();
+            $roleID = $roleRow["roleID"];
+
+
+        $sql = "INSERT INTO user (fname, lname, gender, major, yeargroup, email, passwd) 
+            VALUES ('$fname', '$lname', '$gender', '$major', '$yeargroup', '$email', '$password')";
+        if ($conn->query($sql) === TRUE) {
+
+            if ($roleID == 0 || $roleID == 1) {
+                header("Location: ../g_view/admin.php");
+                exit();
+            } elseif ($roleID == 2 ) {
+                header("Location: ../view/dash.php");
+                exit();
+            } else {
+                echo "Invalid role selected.";
+            }
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
     } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
+        echo "Error: Role not found.";
+        echo "Error: " . $roleQuery . "<br>" . $conn->error;
     }
-
+} else {
+    echo "Error:  role not found.";
+    echo "Error: " . $roleQuery . "<br>" . $conn->error;
+}
 }
 
 $conn->close();
+            
+
