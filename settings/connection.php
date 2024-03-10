34a49071cbfb039d545531962886c51bff42b1fb
Hannahpohmah <111054206+Hannahpohmah@users.noreply.github.com>
2024-03-10 14:00:00 +0000

    Update connection.php

--- settings/connection.php
+++ settings/connection.php
@@ -1,7 +1,7 @@
 <?php      
     $host = "localhost";  
     $user = "root";  
-    $password = '';  
+    $password = 'cs341webtech';  
     $db_name = "natembea";  
       
     $conn = mysqli_connect($host, $user, $password, $db_name);  
